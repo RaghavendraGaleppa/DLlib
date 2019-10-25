8970b4966d3ced409c50e10e2cd654b80127eeec
raghavendra galeppa <Raghavendrar404@gmail.com>
2019-10-25 11:00:10 +0530

    Fixed import errors. Added proper import commands to __init__.py file

--- __init__.py
+++ __init__.py
@@ -1,1 +1,6 @@
-''' The init file may contain confings and stuff 
+''' The init file may contain confings and stuff '''
+import os
+import sys
+sys.path.append(os.path.dirname(__file__))
+
+import utils
