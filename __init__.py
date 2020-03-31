6c3bcf639138839726b1d2e361032499a4f525aa
RaghavendraGaleppa <Raghavendrar404@gmail.com>
2020-03-31 22:40:49 +0530

    Commented out init stuff

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 ''' The init file may contain confings and stuff '''
-import os
-import sys
-sys.path.append(os.path.dirname(__file__))
-
-import utils
-from .train import train
+#import os
+#import sys
+#sys.path.append(os.path.dirname(__file__))
+#
+#import utils
+#from .train import train
